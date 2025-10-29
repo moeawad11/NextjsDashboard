9da8625e44af5716bf574e2e8cea70b150f53c64
moeawad11 <moeawad2004@hotmail.com>
2025-10-29 21:21:24 +0200

    Complete Chapter 13 - Handling Errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -22,7 +22,6 @@ export async function fetchRevenue() {
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
     console.log("Data fetch completed after 3 seconds.");
-
     return data;
   } catch (error) {
     console.error("Database Error:", error);
@@ -160,6 +159,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error("Database Error:", error);
